a2eceb59f713ff88de685c77e3317d01e0f21150
JSakian <Jonathan_Sakian@hotmail.com>
2016-01-20 14:41:02 -0600

    Adds Spring Configuration
    .class files; meta data and a test controller

---
+++ capstone/src/com/gitrdone/TestController.java
@@ -0,0 +1,22 @@
+package com.gitrdone;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value="/TestController")
+	public @ResponseBody String testController() {
+		return "TestViewController is working";	
+	}
+
+	@RequestMapping(value="/lotto")
+	public ModelAndView runLottery() {
+		ModelAndView mv = new ModelAndView("lottoResults");
+//		mv.addObject("lotteryParameters", new lottery())
+		return mv;
+	}
+}
