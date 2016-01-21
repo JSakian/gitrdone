f86655e1ec81b2b5ad32e65924956f6a2d7cd2d8
JSakian <Jonathan_Sakian@hotmail.com>
2016-01-20 19:01:21 -0600

    Adds person controller
    PersonController will navigate the user to:
    -to the eventPage.jsp
    -to either the attendeeForm.jsp or volunteerForm.jsp depending on
    "volunteering" boolean literal value
    -to either the attendeeThankyou.jsp or volunteerThankyou.jsp

---
+++ capstone/src/com/gitrdone/PersonController.java
@@ -0,0 +1,65 @@
+package com.gitrdone;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PersonController {
+
+	/**
+	 * @return the events page so the user may select an event
+	 */
+	@RequestMapping(value = "/events")
+		public ModelAndView events() {
+		ModelAndView mv = new ModelAndView("eventPage");
+		//TODO What objects, if any, do I add for the eventPage.jsp?
+		return mv;
+	}
+	
+	/**
+	 * @return attendee form for Person parameter input
+	 */
+	@RequestMapping(value = "/attendee")
+	public ModelAndView attendee() {
+		ModelAndView mv = new ModelAndView ("attendeeForm");
+		Person attendeePerson = new Person();
+		attendeePerson.setVolunteering(false);
+		mv.addObject("attendee", attendeePerson);
+		return mv;
+	}
+
+	/**
+	 * @return volunteer form for Person parameter input
+	 */
+	@RequestMapping(value = "/volunteer")
+	public ModelAndView volunter() {
+		ModelAndView mv = new ModelAndView ("volunteerForm");
+		Person volunteerPerson = new Person();
+		volunteerPerson.setVolunteering(true);
+		mv.addObject("volunteer", volunteerPerson);
+		return mv;
+	}
+	
+	/**
+	 * @return attendee or volunteer specific thank you page
+	 */
+	@RequestMapping(value = "/formSubmission")
+		public ModelAndView processPerson(Person person) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("person", person);
+		
+		if (person.isVolunteering() == true) {
+			mv.setViewName("volunteerThankyou");}
+		
+		if (person.isVolunteering() == false) {
+			mv.setViewName("attendeeThankyou"); }
+		
+		return mv;
+		//TODO 
+		/*find out if you need an additional method here
+		 * or if you need an additional controller in this
+		 * controller to reroute the user to the "tryAgain.jsp" page...*/
+	}
+
+}
