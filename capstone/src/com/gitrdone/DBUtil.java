577b9d4db9e4a5294db9385460e32c8a7c7e565e
JSakian <Jonathan_Sakian@hotmail.com>
2016-01-26 16:02:56 -0600

    changes database connection & specifies the name of the persistence unit
    unit was  called "jdbc" is now "gitrdoneEventsAndAttendeesDatabase"

--- capstone/src/com/gitrdone/DBUtil.java
+++ capstone/src/com/gitrdone/DBUtil.java
@@ -6,7 +6,7 @@ import javax.persistence.Persistence;
 public class DBUtil {
 	
 	private static final EntityManagerFactory emf = 
-			Persistence.createEntityManagerFactory("jdbc");
+			Persistence.createEntityManagerFactory("gitrdoneEventsAndAttendeesDatabase");
 	
 	public static EntityManagerFactory getEmFactory() {
 		return emf;
