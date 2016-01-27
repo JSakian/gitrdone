932478b832fdecf05f01186f262134e272258f05
Pony936 <pony936@yahoo.com>
2016-01-27 12:40:30 -0600

    added auto wired to service

--- capstone/src/persistence/SerivceImpl.java
+++ capstone/src/persistence/SerivceImpl.java
@@ -2,10 +2,26 @@ package persistence;
 
 import java.util.List;
 
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.gitrdone.Event;
+import com.gitrdone.EventDAOImpl;
+import com.gitrdone.PersonDAOImpl;
 
 public class SerivceImpl implements ServiceInt {
+	
+	@Autowired
+	EntityManagerFactory emf;
+	
+	@Autowired 
+	PersonDAOImpl persons;
+	
+	@Autowired 
+	EventDAOImpl events;
 
+	@Override
 	public List<Event> getAllEvents(){
 		return getAllEvents();
 	}
