2d4f1d2f7c3b6c3d455eb3065bb90198fc7804b5
Pony936 <pony936@yahoo.com>
2016-01-27 12:31:51 -0600

    Service Interface added
    Updated and added to a persistence package.

---
+++ capstone/src/persistence/SerivceImpl.java
@@ -0,0 +1,12 @@
+package persistence;
+
+import java.util.List;
+
+import com.gitrdone.Event;
+
+public class SerivceImpl implements ServiceInt {
+
+	public List<Event> getAllEvents(){
+		return getAllEvents();
+	}
+}
---
+++ capstone/src/persistence/ServiceInt.java
@@ -0,0 +1,11 @@
+package persistence;
+
+import java.util.List;
+
+import com.gitrdone.Event;
+
+public interface ServiceInt {
+	
+	List<Event> getAllEvents();
+
+}
